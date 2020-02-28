704d45f18d2a9da6bb6171f6c2d6ab54554b6f4a
csbing86 <csbing86@hotmail.com>
2020-02-28 15:09:23 -0800

    revised

--- R/fast_corr.R
+++ R/fast_corr.R
@@ -1,15 +1,17 @@
+#' Fast correlation analysis
+#'
+#' Read in .csv data and the column names, conduct correlation analysis between
+#' column pairs, output the correlogram
+#'
+#' @param input_path string
+#' @param col_name list
+#' @param out_path string
+#'
+#' @return plot object
+#'
+#' @examples
+#' fast_corr(input_path, col_name = [], out_path)
+
+
 fast_corr <- function(input_path, col_name, out_path) {
-  #' Fast correlation analysis
-  #'
-  #' Read in .csv data and the column names, conduct correlation analysis between
-  #' column pairs, output the correlogram
-  #'
-  #' @param input_path string
-  #' @param col_name list
-  #' @param out_path string
-  #'
-  #' @return plot object
-  #'
-  #' @examples
-  #' fast_corr(input_path, col_name = [], out_path)
 }
